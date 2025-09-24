72cf162dab41bcdd7013120fb7141cf74073f986
MurtazaAfzali13 <murtazaafzali13@gmail.com>
2025-09-24 16:19:43 +0430

    update fetching player

--- app/foodballs/loading.js
+++ app/foodballs/loading.js
@@ -1,6 +1,6 @@
 import classes from "./loading.module.css"
 export default function LoadingMealsData(){
     return <p className={classes.loading}>
-        Feching meals
+        Feching foodball players
     </p>
 }
